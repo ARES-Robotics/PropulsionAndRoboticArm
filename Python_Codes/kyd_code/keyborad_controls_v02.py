597725496e4020af7a4635eb1ff0019db46d8de8
sanujkul <sanujkul@gmail.com>
2019-12-27 01:01:43 +0530

    Made a improved keyboard BASE code

--- Python_Codes/kyd_code/keyborad_controls_v02.py
+++ Python_Codes/kyd_code/keyborad_controls_v02.py
@@ -25,11 +25,16 @@ print('hello2')
 # serialPortWin = '/dev/ttyUSB0'           #FOR WINDOWS
 # serialPortUbuntu = '/dev/ttyACM0'        #FOR UBUNTU
 # ser = serial.Serial(serialPortUbuntu, 9600,timeout=0.005)
+#######################################################################################
+mode = 0;   #0-> Propulsion, 1-> Robotic Arm
+forwardBackwardSpeed = 0;
+leftRightSpeed = 0;
+deltaIncrement = 1;
 
 def sendDatatoRaspi():
     global forwardBackwardSpeed
     global leftRightSpeed
-    stringData = '1,' + str(forwardBackwardSpeed) + ',' + str(leftRightSpeed)
+    stringData = str(mode) + ',' + str(forwardBackwardSpeed) + ',' + str(leftRightSpeed)
     # Sendng this data from socket to the raspberry pi
     s.send(str.encode(stringData))
     # After sending we check if it was recieved or not
@@ -38,38 +43,48 @@ def sendDatatoRaspi():
 ##################################################################################
 ############################### Variables amd functions for speed ################
 ##################################################################################
-forwardBackwardSpeed = 0;
-leftRightSpeed = 0;
 def increaseForwardBackwardSpeed():
     global forwardBackwardSpeed
-    forwardBackwardSpeed = forwardBackwardSpeed + 1;
-    if(forwardBackwardSpeed > 255):
-        forwardBackwardSpeed = 255;
+    global deltaIncrement
+    forwardBackwardSpeed = forwardBackwardSpeed + deltaIncrement;
+    if(forwardBackwardSpeed > 100):
+        forwardBackwardSpeed = 100;
     printSpeeds()
     sendDatatoRaspi()
 
 def decreaseForwardBackwardSpeed():
     global forwardBackwardSpeed
-    forwardBackwardSpeed = forwardBackwardSpeed - 1;
-    if (forwardBackwardSpeed < -255):
-        forwardBackwardSpeed = -255;
+    global deltaIncrement
+    forwardBackwardSpeed = forwardBackwardSpeed - deltaIncrement;
+    if (forwardBackwardSpeed < -100):
+        forwardBackwardSpeed = -100;
     printSpeeds()
     sendDatatoRaspi()
 
 def increaseleftRightSpeed():
     global leftRightSpeed
-    leftRightSpeed = leftRightSpeed + 1;
-    if (leftRightSpeed > 255):
-        leftRightSpeed = 255;
+    global deltaIncrement
+    leftRightSpeed = leftRightSpeed + deltaIncrement;
+    if (leftRightSpeed > 100):
+        leftRightSpeed = 100;
     printSpeeds()
     sendDatatoRaspi()
 
 def decreaseleftRightSpeed():
     global leftRightSpeed
-    leftRightSpeed = leftRightSpeed - 1;
-    if (leftRightSpeed < -255):
-        leftRightSpeed = -255;
+    global deltaIncrement
+    leftRightSpeed = leftRightSpeed - deltaIncrement;
+    if (leftRightSpeed < -100):
+        leftRightSpeed = -100;
     printSpeeds()
+    sendDatatoRaspi()
+
+def stopAllMotors():
+    global leftRightSpeed
+    global forwardBackwardSpeed
+    leftRightSpeed = 0;
+    forwardBackwardSpeed = 0;
+    printSpeeds();
     sendDatatoRaspi()
 ##################################################################################
 ############################### print statements ##################################
@@ -77,13 +92,22 @@ def decreaseleftRightSpeed():
 def printSpeeds():
     global forwardBackwardSpeed
     global leftRightSpeed
-    stringData = '1,' + str(forwardBackwardSpeed) + ',' + str(leftRightSpeed)
+    global mode
+    stringData = str(mode) + ',' + str(forwardBackwardSpeed) + ',' + str(leftRightSpeed)
     print(" - ",stringData)
 
 ##################################################################################
 ############################### keyboard listener commands  ######################
 ##################################################################################
 def on_press(key):
+    global deltaIncrement
+    global mode
+    keyData = str(key)
+    print('pressed val = ',keyData)
+    print('length', len(keyData))
+    #data = '{0}'.format(key) This will work too than str(key)
+    #print('data',data)
+    #print('length of data',len(data))
     if(format(key)=='Key.up'):
         increaseForwardBackwardSpeed();
     elif(format(key)=='Key.down'):
@@ -92,6 +116,49 @@ def on_press(key):
         decreaseleftRightSpeed();
     elif(format(key)=='Key.right'):
         increaseleftRightSpeed();
+    elif(format(key) == 'Key.space'):
+        stopAllMotors();
+    elif(format(key) == 'Key.enter'):
+        mode = mode ^ 1             #Change Mode value
+        print('MODE CHANGED - 0: Propulsion, 1: Robotic Arm')
+    else: #It is a character (length = 10) or a number (length = 3)
+        if(len(keyData) == 3): # If it is a number
+            charNum = keyData[1];
+            num = int(charNum)
+            if(num == 1):
+                deltaIncrement = 1;
+            elif(num == 2):
+                deltaIncrement = 5;
+            elif(num == 3):
+                deltaIncrement = 10;
+
+        elif(len(keyData) == 10): #This means it is a character: Robotic arm codes:
+            shotenKeyData = keyData[7:9];
+            print(shotenKeyData)
+            if(shotenKeyData == '73'): #w
+                print('It is w');
+            elif(shotenKeyData == '58'):#d
+                print('It is d');
+            elif (shotenKeyData == '82'):  # a
+                print('It is a');
+            elif (shotenKeyData == '76'):  # s
+                print('It is s');
+            elif (shotenKeyData == '68'):  # r
+                print('It is r');
+            elif (shotenKeyData == '56'):  # t
+                print('It is t');
+            elif (shotenKeyData == '95'):  # o
+                print('It is o');
+            elif (shotenKeyData == '62'):  # p
+                print('It is p');
+            elif (shotenKeyData == '46'):  # c
+                print('It is c');
+            elif (shotenKeyData == '53'):  # v
+                print('It is v');
+            elif (shotenKeyData == '74'):  # z
+                print('It is z');
+            elif (shotenKeyData == '97'):  # x
+                print('It is x');
 
 
 def on_release(key):
